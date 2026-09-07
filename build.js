c554eb38bcd1e50746fcc4d43d64574cc75b5cc0
mq3ph <manferquim3@gmail.com>
2026-09-07 22:00:44 +0800

    Delete build.js

--- build.js
+++
@@ -1,3 +0,0 @@
-import {build} from 'esbuild';
-await build({entryPoints:['src/admin-client.js'],bundle:true,minify:true,format:'esm',outfile:'public/admin.bundle.js',platform:'browser'});
-console.log('Admin upload client built.');
