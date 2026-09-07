df4ca5f912ec2fcdf45a086c8e7113330fbabdda
mq3ph <manferquim3@gmail.com>
2026-09-07 22:06:16 +0800

    Delete password.js

--- password.js
+++
@@ -1,6 +0,0 @@
-import {passwordHash,token} from '../src/security.js';
-const password=token().slice(0,24);
-console.log('Save this generated admin password in your password manager:',password);
-console.log('Set these ONLY in .env.local or Vercel environment variables:');
-console.log('ADMIN_PASSWORD_HASH='+passwordHash(password));
-console.log('SESSION_SECRET='+token());
